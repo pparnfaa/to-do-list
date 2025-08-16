842ef1994b75856eda832ffa8a538148000e9296
pparnfaa <kumpol.sb@gmail.com>
2025-08-16 21:11:35 +0700

    feat: Add checklist page and edit checklist

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :quests
+
+  resources :quests do
+    member do
+      post :toggle
+    end
+  end
+root "quests#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
